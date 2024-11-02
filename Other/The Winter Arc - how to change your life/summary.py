fcf9f44abe95ee140f8f52a94ce2fa5e6a974390
KamilMatejuk <kamilmatejuk@gmail.com>
2024-11-02 10:01:15 +0100

    generate october summary

--- Other/The Winter Arc - how to change your life/summary.py
+++ Other/The Winter Arc - how to change your life/summary.py
@@ -1,6 +1,5 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import matplotlib.gridspec as gridspec
 
 
 COLOR_SUCCESSRATE_GREY = '#b0b0b0'
@@ -18,7 +17,13 @@ def time_to_float(time: str) -> float:
     return total
 
 
-def bars_with_threshold(ax1: plt.Axes, ax2: plt.Axes, threshold: str | int,
+def float_to_time(time: float) -> str:
+    h = int(time / 60)
+    m = int(time - h * 60)
+    return ((f'{h}h' if h else '') + ' ' + (f'{m}m' if m else '')).strip()
+
+
+def bars_with_threshold(ax: plt.Axes, label: str, threshold: str | int,
                         values: list[str | int], color: str,
                         values2: list[str | int] = None, color2: str = None,
                         is_time: bool = True, threshold_is_min: bool = True) -> None:
@@ -28,90 +33,110 @@ def bars_with_threshold(ax1: plt.Axes, ax2: plt.Axes, threshold: str | int,
         threshold = time_to_float(threshold)
     total_values = [v1 + v2 for v1, v2 in zip(values, values2)] if values2 else values
 
-    ax1.bar(range(len(values)), values, color=color)
-    if values2: ax1.bar(range(len(values2)), values2, bottom=values, color=color2, label='nap')
-    if values2: ax1.legend()
-    ax1.axhline(threshold, color='red', linestyle='dotted')
-    ax1.set_xticks(range(len(values)))
-    ax1.set_xlim(-0.5, len(values) - 0.5)
-    ax1.set_xticklabels(range(1, 1 + len(values)))
+    ax.bar(range(len(values)), values, color=color)
+    if values2: ax.bar(range(len(values2)), values2, bottom=values, color=color2, label='nap')
+    if values2: ax.legend()
+    ax.set_xticks(range(len(values)))
+    ax.set_xlim(-0.5, len(values) - 0.5)
+    ax.set_xticklabels(range(1, 1 + len(values)))
+    ax.set_title(label)
     if is_time:
         if (max(total_values) // 60) > 1:
             ticks = range(1, int(max(total_values) // 60) + 1)
             if len(ticks) > 7: ticks = ticks[::2]
-            ax1.set_yticks([60 * i for i in ticks])
-            ax1.set_yticklabels([f'{i}h' for i in ticks])
+            ax.set_yticks([60 * i for i in ticks])
+            ax.set_yticklabels([f'{i}h' for i in ticks])
         else:
             ticks = range(1, int(max(total_values) // 5) + 1)
             if len(ticks) > 7: ticks = ticks[::2]
-            ax1.set_yticks([5 * i for i in ticks])
-            ax1.set_yticklabels([f'{5*i}m' for i in ticks])
-    ax1.spines['top'].set_visible(False)
-    ax1.spines['right'].set_visible(False)
+            ax.set_yticks([5 * i for i in ticks])
+            ax.set_yticklabels([f'{5*i}m' for i in ticks])
+    ax.spines['top'].set_visible(False)
+    ax.spines['right'].set_visible(False)
+    
+    # threshold and average
+    avg = int(sum(total_values) / len(total_values))
+    label_thd = ('minimum ' if threshold_is_min else 'maximum ') + (float_to_time(threshold) if is_time else f'{threshold:.0f}') + ' daily'
+    label_avg = f'average ' + (float_to_time(avg) if is_time else f'{avg:.0f}') + ' daily'
+    ax.axhline(threshold, color='#023020', linestyle='dotted')
+    ax.axhline(avg, color='purple', linestyle='dotted')
+    # fix overlapping texts
+    dist = abs(threshold - avg) / max(total_values)
+    if dist < 0.15:
+        padd = (0.15 * max(total_values) - abs(threshold - avg)) / 2
+        if threshold > avg: threshold_padded, avg_padded = threshold + padd, avg - padd
+        else: threshold_padded, avg_padded = threshold - padd, avg + padd
+    else: threshold_padded, avg_padded = threshold, avg
+    ax.text(x=len(total_values), y=threshold_padded, s=label_thd, ha='left', va='center', fontsize=12, color='#023020')
+    ax.text(x=len(total_values), y=avg_padded, s=label_avg, ha='left', va='center', fontsize=12, color='purple')
 
     if threshold_is_min:
         success = sum(v >= threshold for v in total_values)
     else:
         success = sum(v <= threshold for v in total_values)
-    wedges, _ = ax2.pie([success, len(total_values) - success], startangle=90,
-                        colors=[COLOR_SUCCESSRATE_MAIN, COLOR_SUCCESSRATE_GREY])
-    wedges[0].set(width=0.3)
-    wedges[1].set(width=0.3, linewidth=8, edgecolor='white')
-    ax2.text(0, 0, f'{success/len(total_values):.0%}', ha='center', va='center', fontsize=16)
-    ax2.text(0, -1.3, f'success rate', ha='center', va='center', fontsize=12)
+    return success/len(total_values)
 
 
 if __name__ == '__main__':
     rows = 6
     colors = [plt.cm.winter(i) for i in np.linspace(0, 1, rows + 1)]
+    
+    labels = ['Side projects', 'Reading', 'Exercise', 'Sleep', 'Calories', 'Phone usage',
+              'Articles', 'Gym', 'Alcohol']
 
-    fig = plt.figure(figsize=(10, 2 * rows))
-    gs = gridspec.GridSpec(rows, 2, width_ratios=[4, 1])
+    fig, axes = plt.subplots(rows, 1, figsize=(12, 2 * rows))
 
-    ax1 = fig.add_subplot(gs[0, 0])
-    ax2 = fig.add_subplot(gs[0, 1])
-    ax1.set_title('Side projects (minimum 15m daily)', fontsize=14)
-    bars_with_threshold(ax1, ax2, '15m', ['15m', '1h', '1h', '30m', '2h', '2h', '3h', '0', '0',
-        '2h', '30m', '5h', '2,5h', '0', '0', '15m', '3h', '0', '0', '0', '0', '0', '1h', '1h'],
-        colors[0])
+    passrate_projects = bars_with_threshold(axes[0], labels[0], '15m',
+        ['15m', '1h', '1h', '30m', '2h', '2h', '3h', '0', '0', '2h', '30m', '5h', '2,5h', '0',
+        '0', '15m', '3h', '0', '0', '0', '0', '0', '1h', '1h', '2.5h', '3h', '2h', '5m', '0',
+        '0', '30m'], colors[0])
 
-    ax1 = fig.add_subplot(gs[1, 0])
-    ax2 = fig.add_subplot(gs[1, 1])
-    ax1.set_title('Reading (minimum 5m daily)', fontsize=14)
-    bars_with_threshold(ax1, ax2, '5m', ['15m', '15m', '15m', '15m', '15m', '15m', '15m', '15m',
-        '15m', '15m', '15m', '15m', '15m', '15m', '5m', '0', '15m', '15m', '0', '0', '15m', '15m',
-        '15m', '0'], colors[1])
+    passrate_reading = bars_with_threshold(axes[1], labels[1], '5m',
+        ['20m', '15m', '15m', '25m', '10m', '15m', '20m', '5m', '15m', '30m', '15m', '20m', '10m',
+        '10m', '5m', '0', '15m', '20m', '0', '0', '15m', '15m', '15m', '0', '0', '15m', '20m',
+        '15m', '0', '0', '20m'], colors[1])
 
-    ax1 = fig.add_subplot(gs[2, 0])
-    ax2 = fig.add_subplot(gs[2, 1])
-    ax1.set_title('Exercise (minimum 10m daily)', fontsize=14)
-    bars_with_threshold(ax1, ax2, '10m', ['2.5h', '20m', '1.5h', '1h', '0', '3h', '0', '3h', '5m',
-        '0', '2.5h', '20m', '0', '1h', '1.5h', '1h', '0', '2h', '4h', '0', '0', '2.5h', '10m',
-        '1.5h'], colors[2])
+    passrate_exercise = bars_with_threshold(axes[2], labels[2], '10m',
+        ['2.5h', '20m', '1.5h', '1h', '0', '3h', '0', '3h', '5m', '0', '2.5h', '20m', '0', '1h',
+        '1.5h', '1h', '0', '2h', '4h', '0', '0', '2.5h', '10m', '1.5h', '1h', '0', '1h', '0',
+        '0', '20m', '0'], colors[2])
 
-    ax1 = fig.add_subplot(gs[3, 0])
-    ax2 = fig.add_subplot(gs[3, 1])
-    ax1.set_title('Sleep (minimum 7h daily)', fontsize=14)
-    bars_with_threshold(ax1, ax2, '7h', ['7h', '6h', '7,5h', '5h', '6h', '10h', '6.5h', '5,5h',
-        '6,5h', '7,5h', '5,5h', '9h', '7,5h', '6h', '6,5h', '9h', '7h', '7h', '7,5h', '11h', '5h',
-        '5,5h', '7h', '7h'], colors[3], ['0', '0', '0', '3h', '0', '0', '2h', '0', '0', '3h',
-        '1,5h', '3h', '0', '0', '0', '0', '0', '2h', '0', '2h', '3h', '0', '0', '1h'], colors[4])
+    passrate_sleep = bars_with_threshold(axes[3], labels[3], '7h',
+        ['7h', '6h', '7,5h', '5h', '6h', '10h', '6.5h', '5,5h', '6,5h', '7,5h', '5,5h', '9h',
+        '7,5h', '6h', '6,5h', '9h', '7h', '7h', '7,5h', '11h', '5h', '5,5h', '7h', '7h', '6.5h',
+        '7h', '8h', '8h', '3.5h', '10h', '6h'], colors[3],
+        ['0', '0', '0', '3h', '0', '0', '2h', '0', '0', '3h', '1,5h', '3h', '0', '0', '0', '0', '0',
+        '2h', '0', '2h', '3h', '0', '0', '1h', '0', '4h', '0', '2h', '4h', '2h', '0'], colors[4])
 
-    ax1 = fig.add_subplot(gs[4, 0])
-    ax2 = fig.add_subplot(gs[4, 1])
-    ax1.set_title('Calories (minimum 2900 kcal daily)', fontsize=14)
-    bars_with_threshold(ax1, ax2, 2900, [1826, 3179, 2783, 2145, 2361, 2222, 2325, 1796, 2860, 2789,
-        2537, 3148, 2458, 2756, 2224, 2898, 2629, 2131, 0, 1487, 2349, 2822, 3640, 2465], colors[5],
-        is_time=False)
+    passrate_calories = bars_with_threshold(axes[4], labels[4], 2900,
+        [1826, 3179, 2783, 2145, 2361, 2222, 2325, 1796, 2860, 2789, 2537, 3148, 2458, 2756, 2224,
+        2898, 2629, 2131, 0, 1487, 2349, 2822, 3640, 2465, 3578, 2306, 3016, 3233, 2395, 2480],
+        colors[5], is_time=False)
 
-    ax1 = fig.add_subplot(gs[5, 0])
-    ax2 = fig.add_subplot(gs[5, 1])
-    ax1.set_title('Phone usage (maximum 4h daily)', fontsize=14)
-    bars_with_threshold(ax1, ax2, '4h', ['3h 32m', '5h 19m', '3h 11m', '4h 30m', '1h 4m', '2h 29m',
-        '3h 17m', '4h 55m', '6h 8m', '4h 26m', '3h 57m', '54m', '2h 36m', '4h 11m', '3h 25m',
-        '3h 17m', '3h 48m', '3h 25m', '1h 13m', '1h 4m', '4h 26m', '3h 57m', '3h 31m', '2h 19m'],
-        colors[6], threshold_is_min=False)
+    passrate_phone = bars_with_threshold(axes[5], labels[5], '4h',
+        ['3h 32m', '5h 19m', '3h 11m', '4h 30m', '1h 4m', '2h 29m', '3h 17m', '4h 55m', '6h 8m',
+        '4h 26m', '3h 57m', '54m', '2h 36m', '4h 11m', '3h 25m', '3h 17m', '3h 48m', '3h 25m',
+        '1h 13m', '1h 4m', '4h 26m', '3h 57m', '3h 31m', '2h 19m', '3h 32m', '45m', '1h 34m',
+        '3h 6m', '3h 31m', '2h 57m', '4h 2m'], colors[6], threshold_is_min=False)
 
     plt.tight_layout()
-    fig.subplots_adjust(hspace=0.75)
-    plt.savefig('summary.png')
+    plt.savefig('summary_timeline.png')
+    
+
+    fig, ax = plt.subplots(figsize=(12, 8), subplot_kw=dict(polar=True))
+    passrates = [passrate_projects, passrate_reading, passrate_exercise, passrate_sleep, passrate_calories,
+                 passrate_phone, 0.50, 0.75, 0.25]
+    angles = np.linspace(0, 2 * np.pi, len(passrates), endpoint=False).tolist()
+    angles += angles[:1]
+    ax.fill(angles, passrates + passrates[:1], color=colors[4], alpha=0.25)
+    ax.plot(angles, passrates + passrates[:1], color=colors[4], linewidth=2)
+    ax.set_ylim(0, 1)
+    ax.set_yticklabels([])
+    ax.set_xticks(angles[:-1])
+    ax.set_xticklabels([])
+    for i, (label, value, angle) in enumerate(zip(labels, passrates, angles)):
+        ha = 'right' if np.pi * 0.5 < angle < np.pi * 1.5 else 'left'
+        ax.text(angle, 1.1, label, ha=ha, va='center', fontsize=12)
+        ax.text(angle, value + 0.07, f"{value:.0%}", ha='center', va='center', fontsize=12)
+    ax.text(0, 0, f"Overall\n{np.mean(passrates):.2%}", ha='center', va='center', fontsize=14, fontweight="bold")
+    plt.savefig('summary_passrates.png')
