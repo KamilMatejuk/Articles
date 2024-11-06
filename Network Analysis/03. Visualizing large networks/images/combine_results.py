5f39059f5fe17593f13ed4532d35694928a0feb0
KamilMatejuk <kamilmatejuk@gmail.com>
2024-11-06 14:23:38 +0100

    move all img generation based on results images into one script

---
+++ Network Analysis/03. Visualizing large networks/images/combine_results.py
@@ -0,0 +1,28 @@
+import re
+import glob
+import matplotlib.pyplot as plt
+
+
+def generate(prefix: str, orders: list[int] = None):
+    files = sorted(list(glob.glob(f'../results/{prefix}_*.png')))
+    if orders is not None:
+        files = [files[i] for i in orders]
+
+    fig, axes = plt.subplots(1, len(files), figsize=(12, 12 / len(files) + 0.5))
+
+    for i, filename in enumerate(files):
+        label = re.search(rf'{prefix}_(.+).png', filename).group(1).title()
+        axes[i].imshow(plt.imread(filename))    
+        axes[i].set_title(label)
+    
+    for ax in axes: ax.axis('off')
+    fig.tight_layout()
+    plt.savefig(f'combined_results_{prefix}.png')
+
+
+if __name__ == '__main__':
+    # generate('basic', orders=[2, 1, 0, 3])
+    # generate('nx', orders=[1, 0, 2, 3, 4])
+    # generate('edge_visibility_based_on_degree', orders=[1, 0, 2, 3])
+    # generate('edge_visibility_based_on_degree_exp', orders=[1, 0, 2, 3])
+    pass
