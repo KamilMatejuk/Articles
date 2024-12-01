986e141b4a8a9ac21c59be992d6cfa36b986af32
KamilMatejuk <kamilmatejuk@gmail.com>
2024-12-01 15:38:19 +0100

    generate november summary

--- Other/The Winter Arc - how to change your life/summary.py
+++ Other/The Winter Arc - how to change your life/summary.py
@@ -87,42 +87,42 @@ if __name__ == '__main__':
     colors = [plt.cm.winter(i) for i in np.linspace(0, 1, rows + 1)]
     
     labels = ['Side projects', 'Reading', 'Exercise', 'Sleep', 'Calories', 'Phone usage',
-              'Articles', 'Gym', 'Alcohol']
+              'Articles', 'Gym', 'Shoulder raises', 'Alcohol']
 
     fig, axes = plt.subplots(rows, 1, figsize=(12, 2 * rows))
 
     passrate_projects = bars_with_threshold(axes[0], labels[0], '15m',
-        ['15m', '1h', '1h', '30m', '2h', '2h', '3h', '0', '0', '2h', '30m', '5h', '2,5h', '0',
-        '0', '15m', '3h', '0', '0', '0', '0', '0', '1h', '1h', '2.5h', '3h', '2h', '5m', '0',
-        '0', '30m'], colors[0])
+        ['30m', '3h', '2h', '30m', '1h 45m', '2,5h', '4h', '3h', '4h', '0', '2h', '1.5h', '0', '0',
+        '1,5h', '0', '0', '0', '0.5h', '15m', '2h', '2.5h', '0', '0', '0,5h', '2h', '15m', '4,5h',
+        '15m', '0'], colors[0])
 
     passrate_reading = bars_with_threshold(axes[1], labels[1], '5m',
-        ['20m', '15m', '15m', '25m', '10m', '15m', '20m', '5m', '15m', '30m', '15m', '20m', '10m',
-        '10m', '5m', '0', '15m', '20m', '0', '0', '15m', '15m', '15m', '0', '0', '15m', '20m',
-        '15m', '0', '0', '20m'], colors[1])
+        ['25m', '50m', '0', '30m', '35m', '0', '0', '10m', '0', '0', '10m', '25m', '10m', '0', '0',
+        '0', '10m', '0', '10m', '10m', '20m', '5m', '15m', '5m', '20m', '10m', '20m', '10m', '10m',
+        '5m'], colors[1])
 
     passrate_exercise = bars_with_threshold(axes[2], labels[2], '10m',
-        ['2.5h', '20m', '1.5h', '1h', '0', '3h', '0', '3h', '5m', '0', '2.5h', '20m', '0', '1h',
-        '1.5h', '1h', '0', '2h', '4h', '0', '0', '2.5h', '10m', '1.5h', '1h', '0', '1h', '0',
-        '0', '20m', '0'], colors[2])
+        ['1h', '1h', '0', '15m', '1.5h', '1h', '1.5h', '1,5h', '0', '1h', '10m', '15m', '1h',
+        '1,5h', '2h', '0', '0', '1h', '1.5h', '1h', '1.5h', '2h', '0', '0', '0', '1.5h', '10m',
+        '10m', '1h', '0'], colors[2])
 
     passrate_sleep = bars_with_threshold(axes[3], labels[3], '7h',
-        ['7h', '6h', '7,5h', '5h', '6h', '10h', '6.5h', '5,5h', '6,5h', '7,5h', '5,5h', '9h',
-        '7,5h', '6h', '6,5h', '9h', '7h', '7h', '7,5h', '11h', '5h', '5,5h', '7h', '7h', '6.5h',
-        '7h', '8h', '8h', '3.5h', '10h', '6h'], colors[3],
-        ['0', '0', '0', '3h', '0', '0', '2h', '0', '0', '3h', '1,5h', '3h', '0', '0', '0', '0', '0',
-        '2h', '0', '2h', '3h', '0', '0', '1h', '0', '4h', '0', '2h', '4h', '2h', '0'], colors[4])
+        ['8h', '8h', '7h', '6h', '5h', '7h', '6h', '5h', '6,5h', '6h', '8h', '8h', '8h', '7,5h',
+        '7h', '6,5h', '12h', '7,5h', '7,5h', '4h', '7h', '7h', '9h', '8h', '5h', '5,5h', '7h',
+        '6h', '5,5h', '6h'], colors[3],
+        ['0', '0', '0', '3h', '2h', '0', '0', '0', '3h', '3h', '0,5h', '0', '0', '0', '0', '0',
+        '0', '0', '0', '3h', '0', '1h', '0', '0', '2.5h', '0', '2,5h', '0', '3,5h', '0'], colors[4])
 
     passrate_calories = bars_with_threshold(axes[4], labels[4], 2900,
-        [1826, 3179, 2783, 2145, 2361, 2222, 2325, 1796, 2860, 2789, 2537, 3148, 2458, 2756, 2224,
-        2898, 2629, 2131, 0, 1487, 2349, 2822, 3640, 2465, 3578, 2306, 3016, 3233, 2395, 2480],
+        [3373, 2735, 3311, 3031, 2906, 2936, 3258, 2949, 2192, 2760, 2965, 2946, 2783, 3002, 2764,
+        0, 3402, 2907, 3014, 3147, 3074, 2958, 0, 0, 2627, 3349, 3191, 3085, 3650, 0],
         colors[5], is_time=False, omit_zeros_in_avg=True)
 
-    passrate_phone = bars_with_threshold(axes[5], labels[5], '4h',
-        ['3h 32m', '5h 19m', '3h 11m', '4h 30m', '1h 4m', '2h 29m', '3h 17m', '4h 55m', '6h 8m',
-        '4h 26m', '3h 57m', '54m', '2h 36m', '4h 11m', '3h 25m', '3h 17m', '3h 48m', '3h 25m',
-        '1h 13m', '1h 4m', '4h 26m', '3h 57m', '3h 31m', '2h 19m', '3h 32m', '45m', '1h 34m',
-        '3h 6m', '3h 31m', '2h 57m', '4h 2m'], colors[6], threshold_is_min=False)
+    passrate_phone = bars_with_threshold(axes[5], labels[5], '3h 30m',
+        ['1h 21m', '2h 23m', '2h 27m', '4h 28m', '3h 55m', '5h 25m', '2h 24m', '3h 8m', '2h 44m',
+        '1h 23m', '2h 58m', '4h 22m', '3h 2m', '3h 34m', '4h 54m', '52m', '32m', '4h 10m',
+        '3h 13m', '2h 56m', '3h 8m', '2h 33m', '59m', '1h 28m', '4h 1m', '2h 51m', '2h 38m',
+        '3h 45m', '1h 57m', '1h 4m'], colors[6], threshold_is_min=False)
 
     plt.tight_layout()
     plt.savefig('summary_timeline.png')
@@ -130,7 +130,8 @@ if __name__ == '__main__':
 
     fig, ax = plt.subplots(figsize=(12, 8), subplot_kw=dict(polar=True))
     passrates = [passrate_projects, passrate_reading, passrate_exercise, passrate_sleep, passrate_calories,
-                 passrate_phone, 0.50, 0.75, 0.25]
+                 passrate_phone, 0.50, 0.75, 0.70, 0.50]
+    improvements = [9, -7, 12, 9, 47, -4, 0, 0, 0, 25]
     angles = np.linspace(0, 2 * np.pi, len(passrates), endpoint=False).tolist()
     angles += angles[:1]
     ax.fill(angles, passrates + passrates[:1], color=colors[4], alpha=0.25)
@@ -139,9 +140,14 @@ if __name__ == '__main__':
     ax.set_yticklabels([])
     ax.set_xticks(angles[:-1])
     ax.set_xticklabels([])
-    for i, (label, value, angle) in enumerate(zip(labels, passrates, angles)):
+    for i, (label, value, improvement, angle) in enumerate(zip(labels, passrates, improvements, angles)):
         ha = 'right' if np.pi * 0.5 < angle < np.pi * 1.5 else 'left'
         ax.text(angle, 1.1, label, ha=ha, va='center', fontsize=12)
-        ax.text(angle, value + 0.07, f"{value:.0%}", ha='center', va='center', fontsize=12)
+        text_kwargs = dict(ha='center', va='center', fontsize=12)
+        text = ax.text(angle, value + 0.07, f"{value:.0%}", **text_kwargs)
+        if improvement > 0:
+            ax.annotate(f"\n(+{improvement}%)", xycoords=text, xy=(0.5, 0), color="green", **text_kwargs)
+        if improvement < 0:
+            ax.annotate(f"\n({improvement}%)", xycoords=text, xy=(0.5, 0), color="red", **text_kwargs)
     ax.text(0, 0, f"Overall\n{np.mean(passrates):.2%}", ha='center', va='center', fontsize=14, fontweight="bold")
     plt.savefig('summary_passrates.png')
