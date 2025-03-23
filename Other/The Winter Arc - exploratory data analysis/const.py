3e94ab9999f76fcb2aaa3ebd6c48304b33953e36
KamilMatejuk <kamilmatejuk@gmail.com>
2025-03-23 22:43:56 +0100

    const

---
+++ Other/The Winter Arc - exploratory data analysis/const.py
@@ -0,0 +1,9 @@
+COLS_SLEEPING = ['sleep_night_time', 'sleep_day_time', 'sleep_total_time']
+COLS_READING = ['read_book_time', 'read_crossword_time']
+COLS_WORKING = ['side_projects_time']
+COLS_LEISURE = ['phone_time', 'tv_time']
+COLS_EXERCISE = ['exercise_volleyball_time', 'exercise_gym_time', 'exercise_home_time', 'exercise_swim_time', 'exercise_run_time', 'exercise_dance_time',
+                 'exercise_shoulder_raises_series', 'exercise_shoulder_raises_avg_reps_per_serie', 'exercise_shoulder_raises_avg_weight_per_serie']
+COLS_EATING = ['macros_protein', 'macros_carbs', 'macros_fat', 'macros_alcohol', 'macros_kcal']
+COLS_PUBLISHING = ['published_medium', 'published_linkedin']
+COLS_OTHER = ['date', 'week_day', 'working', 'out_of_home', 'energy']
