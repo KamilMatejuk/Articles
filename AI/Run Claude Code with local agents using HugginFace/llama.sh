166fc7d0006ee5fe2790eb23700f565a640efc51
KamilMatejuk <kamilmatejuk@gmail.com>
2026-04-10 02:43:20 +0200

    use Gemma 4 model

--- AI/Run Claude Code with local agents using HugginFace/llama.sh
+++ AI/Run Claude Code with local agents using HugginFace/llama.sh
@@ -14,24 +14,16 @@ serve() {
         --flash-attn on \
         --batch-size 2048 \
         --ubatch-size 512 \
-        --ctx-size 32768
+        --parallel 1 \
+        --ctx-size 131072
+
+    # Qwen has context of 32k (32,768)
+    # GPT-OSS has context of 128k (131,072)
+    # Gemma4 has context of 128k (131,072)
 }
 
 
-# serve bartowski/DeepSeek-Coder-V2-Lite-Instruct-GGUF Q4_K_M DeepSeek-Coder-Q4
-# serve bartowski/DeepSeek-Coder-V2-Lite-Instruct-GGUF Q8_0 DeepSeek-Coder-Q8
-
-# serve unsloth/Qwen3-Coder-Next-GGUF UD-TQ1_0 Qwen3-Coder-Next-Q1
-# serve unsloth/Qwen3-Coder-Next-GGUF UD-IQ2_XXS Qwen3-Coder-Next-Q2
-# serve unsloth/Qwen3-Coder-Next-GGUF IQ4_NL Qwen3-Coder-Next-Q4
-
-# serve unsloth/Qwen3-Coder-30B-A3B-Instruct-GGUF Q3_K_M Qwen3-Coder-Q3
-# serve unsloth/Qwen3-Coder-30B-A3B-Instruct-GGUF Q4_0 Qwen3-Coder-Q4
-
-# serve unsloth/Qwen3.5-9B-GGUF IQ4_NL Qwen3.5-Q4
-# serve unsloth/Qwen3.5-9B-GGUF Q8_0 Qwen3.5-Q8
-# serve unsloth/Qwen3.5-9B-GGUF UD-Q8_K_XL Qwen3.5-U8
-
-# serve unsloth/gpt-oss-20b-GGUF Q4_K_M GPT-OSS-Q4
-serve unsloth/gpt-oss-20b-GGUF Q8_0 GPT-OSS-Q8
-# serve unsloth/gpt-oss-20b-GGUF F16 GPT-OSS-F16
+# serve unsloth/Qwen3.5-9B-GGUF IQ4_NL Qwen3.5-Q8
+# serve unsloth/gpt-oss-20b-GGUF Q8_0 GPT-OSS-Q8
+# serve unsloth/gemma-4-E4B-it-GGUF BF16 Gemma4 # 15.1 GB
+serve unsloth/gemma-4-E4B-it-GGUF Q8_0 Gemma4 # 8.19 GB
